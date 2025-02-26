d8d057681d2203b80ad4bf1a17843d9750104c09
msploit0x00 <minasploit1@outlook.com>
2025-02-26 11:43:32 +0200

    purchase receipt duplicate

---
+++ shipment_order/shipment_order/doctype/shipments_orders/shipments_orders.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, shipment_order and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Shipments Orders", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ shipment_order/shipment_order/doctype/shipments_orders/test_shipments_orders.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, shipment_order and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestShipmentsOrders(FrappeTestCase):
+	pass
